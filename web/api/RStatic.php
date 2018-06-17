882fdabb0f8cbb34e8719e65349465a14c0ca550
wellprog <velorkhalilov@gmail.com>
2018-06-17 12:53:27 +0300

    Add add record

--- web/api/RStatic.php
+++ web/api/RStatic.php
@@ -1,6 +1,24 @@
 <?php
 require dirname(__FILE__) . "/../link/link.php";
 
+function addRecord() {
+    $model = Vtiger_Record_Model::getCleanInstance("RStatic");
+
+    $model->set("name", $_POST["name"]);
+    $model->set("text", $_POST["text"]);
+    $model->set("shorttext", $_POST["shorttext"]);
+
+    if ($model->save())
+        return true;
+    
+    return false;
+}
+
+if (isset($_POST["method"])) {
+    if ($_POST["method"] == "add")
+        return addRecord();
+}
+
 $requester = new link();
 $query = $requester->getLink("RStatic")->createQuery();
 
