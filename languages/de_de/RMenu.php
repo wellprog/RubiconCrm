6bb9db97ea7be4c02823fb8c1c89357fa452450b
root <root@velorkhalilov.fvds.ru>
2018-07-08 08:39:04 +0000

    from server

---
+++ languages/de_de/RMenu.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * ModuleName english translation
+ * @package YetiForce.Languages
+ * @copyright YetiForce Sp. z o.o.
+ * @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ */
+$languageStrings = [
+	'RMenu' => 'Меню сайта',
+	'SINGLE_RMenu' => 'Меню сайта',
+	//BLOCKS
+	'LBL_CUSTOM_INFORMATION' => 'Custom Information',
+	'LBL_BASIC_INFORMATION' => 'Basic Information',
+	//FIELDS
+	'name' => 'Наименование',
+];
---
+++ modules/RMenu/RMenu.php
@@ -0,0 +1,98 @@
+<?php
+/* +**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ * Contributor(s): YetiForce.com
+ * ********************************************************************************** */
+
+include_once 'modules/Vtiger/CRMEntity.php';
+
+class RMenu extends Vtiger_CRMEntity
+{
+
+	public $table_name = 'vtiger_rmenu';
+	public $table_index = 'rmenuid';
+
+	/**
+	 * Mandatory table for supporting custom fields.
+	 */
+	public $customFieldTable = ['vtiger_rmenucf', 'rmenuid'];
+
+	/**
+	 * Mandatory for Saving, Include tables related to this module.
+	 */
+	public $tab_name = ['vtiger_crmentity', 'vtiger_rmenu', 'vtiger_rmenucf'];
+
+	/**
+	 * Mandatory for Saving, Include tablename and tablekey columnname here.
+	 */
+	public $tab_name_index = [
+		'vtiger_crmentity' => 'crmid',
+		'vtiger_rmenu' => 'rmenuid',
+		'vtiger_rmenucf' => 'rmenuid'
+	];
+
+	/**
+	 * Mandatory for Listing (Related listview)
+	 */
+	public $list_fields = [
+		/* Format: Field Label => Array(tablename, columnname) */
+		// tablename should not have prefix 'vtiger_'
+		'Наименование' => ['rmenu', 'name'],
+		'Assigned To' => ['crmentity', 'smownerid']
+	];
+	public $list_fields_name = [
+		/* Format: Field Label => fieldname */
+		'Наименование' => 'name',
+		'Assigned To' => 'assigned_user_id',
+	];
+	// Make the field link to detail view
+	public $list_link_field = 'name';
+	// For Popup listview and UI type support
+	public $search_fields = [
+		/* Format: Field Label => Array(tablename, columnname) */
+		// tablename should not have prefix 'vtiger_'
+		'Наименование' => ['rmenu', 'name'],
+		'Assigned To' => ['vtiger_crmentity', 'assigned_user_id'],
+	];
+	public $search_fields_name = [
+		/* Format: Field Label => fieldname */
+		'Наименование' => 'name',
+		'Assigned To' => 'assigned_user_id',
+	];
+	// For Popup window record selection
+	public $popup_fields = ['name'];
+	// For Alphabetical search
+	public $def_basicsearch_col = 'name';
+	// Column value to use on detail view record text display
+	public $def_detailview_recname = 'name';
+	// Used when enabling/disabling the mandatory fields for the module.
+	// Refers to vtiger_field.fieldname values.
+	public $mandatory_fields = ['name', 'assigned_user_id'];
+	public $default_order_by = '';
+	public $default_sort_order = 'ASC';
+
+	/**
+	 * Invoked when special actions are performed on the module.
+	 * @param String Module name
+	 * @param String Event Type
+	 */
+	public function moduleHandler($moduleName, $eventType)
+	{
+		if ($eventType === 'module.postinstall') {
+			
+		} else if ($eventType === 'module.disabled') {
+			
+		} else if ($eventType === 'module.preuninstall') {
+			
+		} else if ($eventType === 'module.preupdate') {
+			
+		} else if ($eventType === 'module.postupdate') {
+			
+		}
+	}
+}
